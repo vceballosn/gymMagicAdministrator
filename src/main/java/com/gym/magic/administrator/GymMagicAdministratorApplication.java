2acf8ebc68d90ccea5bea21f33de8bab104414bc
vceballosn <vladimir.ceballos@gmail.com>
2025-08-07 18:28:10 -0400

    create proyect structure package

---
+++ src/main/java/com/gym/magic/administrator/GymMagicAdministratorApplication.java
@@ -0,0 +1,13 @@
+package com.gym.magic.administrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymMagicAdministratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymMagicAdministratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gym/magic/administrator/gymMagicAdministrator/GymMagicAdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.magic.administrator.gymMagicAdministrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymMagicAdministratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
